5da542e2f8012cdba6fa20226f2d281eeeb7d12c
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-06-29 15:17:36 -0300

    Theme saved in localstorage

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -5,20 +5,24 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({children}) => {
-    const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState(() => {
+    const storedTheme = JSON.parse(localStorage.getItem('PAGE_THEME'));
+    if(!storedTheme) return 'dark';
+    return storedTheme;
+  });
 
-      useEffect(() => {
-        localStorage.setItem('PAGE_THEME', JSON.stringify(theme));
-      }, [theme]);
+  useEffect(() => {
+    localStorage.setItem('PAGE_THEME', JSON.stringify(theme));
+  }, [theme]);
 
-    return (
-        <ThemeContext.Provider value={{
-            theme,
-            toggleTheme: () => setTheme(theme === 'dark' ? 'light' : 'dark'),
-        }}>
-            {children}
-        </ThemeContext.Provider>
-    )
+  return (
+      <ThemeContext.Provider value={{
+          theme,
+          toggleTheme: () => setTheme(theme === 'dark' ? 'light' : 'dark'),
+      }}>
+          {children}
+      </ThemeContext.Provider>
+  )
 };
 
 export const useTheme = () => useContext(ThemeContext);
