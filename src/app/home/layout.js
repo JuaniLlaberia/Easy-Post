fe1350fd8dcfe8c0eb0b8a1bd47f7a77e25f50c1
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-07-01 18:41:36 -0300

    fixed icons bug on refresh

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -1,4 +1,7 @@
 import Sidebar from "@/components/Sidebar";
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 
 export default function HomeLayout({ children }) {
   return (
