74131d779166fada668e418a33887bb0dad2b2b7
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-07-01 11:54:06 -0300

    Sidebar Links refreshing test

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -26,11 +26,11 @@ const Sidebar = () => {
     <>
       <nav className={isActive ? 'active' : ''}>
         <div className="sidebar-btns">
-          <Link className='nav-btn' onClick={() => setIsActive(false)} href='/home'><FontAwesomeIcon icon={faHouse}/> <span className='nav-btn-text'>Home</span></Link>
-          <Link className='nav-btn' onClick={() => setIsActive(false)} href='/home/search'><FontAwesomeIcon icon={faMagnifyingGlass}/> <span className='nav-btn-text'>Search</span></Link>
-          <Link className='nav-btn' onClick={() => setIsActive(false)} href='/home/trending'><FontAwesomeIcon icon={faStar}/> <span className='nav-btn-text'>Trending</span></Link>
-          <Link className='nav-btn' onClick={() => setIsActive(false)} href='/home/inbox'><FontAwesomeIcon icon={faEnvelope}/> <span className='nav-btn-text'>Inbox</span></Link>
-          <Link className='nav-btn' onClick={() => setIsActive(false)} href='/home/my-profile'><FontAwesomeIcon icon={faUser}/> <span className='nav-btn-text'>Profile</span></Link>
+          <Link className='nav-btn' onClick={() => setIsActive(false)} href='home'><FontAwesomeIcon icon={faHouse}/> <span className='nav-btn-text'>Home</span></Link>
+          <Link className='nav-btn' onClick={() => setIsActive(false)} href='home/search'><FontAwesomeIcon icon={faMagnifyingGlass}/> <span className='nav-btn-text'>Search</span></Link>
+          <Link className='nav-btn' onClick={() => setIsActive(false)} href='home/trending'><FontAwesomeIcon icon={faStar}/> <span className='nav-btn-text'>Trending</span></Link>
+          <Link className='nav-btn' onClick={() => setIsActive(false)} href='home/inbox'><FontAwesomeIcon icon={faEnvelope}/> <span className='nav-btn-text'>Inbox</span></Link>
+          <Link className='nav-btn' onClick={() => setIsActive(false)} href='home/my-profile'><FontAwesomeIcon icon={faUser}/> <span className='nav-btn-text'>Profile</span></Link>
         </div>
         <div className='nav-bottom-btns'>
           <Link href='/home/settings' className='more' onClick={() => setIsActive(false)}><FontAwesomeIcon className='more-symbol' icon={faGear}/> <span className='more-text'>Settings</span></Link>
